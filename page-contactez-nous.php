38d4b8d2488c5c9b82cb13d12187f9bda1e5b99a
loubal70 <louisbal70@gmail.com>
2022-05-13 17:05:51 +0200

    MAJ 13/05 Co-authored-by: OmbrouckAlexis <OmbrouckAlexis@users.noreply.github.com> Co-authored-by: BPicquet <BPicquet@users.noreply.github.com>

---
+++ page-contactez-nous.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<section class="template-contact">
+
+<h1 class="title">Contact</h1>
+
+<div class="contact__header">
+    <div>
+        <img src="" alt="Contactez le Lycée cousteau">
+    </div>
+    <div>
+        <h2></h2>
+    </div>
+</div>
+
+</section>
+
+<?php get_footer(); ?>
