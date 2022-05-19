957b59fc54cea70db4dee5d208be720b5e9afbd7
loubal70 <louis.boulanger.work@gmail.com>
2022-05-19 16:00:27 +0200

    MAJ 19/05
    Co-authored-by: BPicquet <BPicquet@users.noreply.github.com>
    Co-authored-by: OmbrouckAlexis <OmbrouckAlexis@users.noreply.github.com>

--- page-espaces-entreprises.php
+++ page-espaces-entreprises.php
@@ -3,13 +3,37 @@
 <div class="template-entreprises content-body container">
     <div class="entreprises__presentation">
         <h1 class="title"><?php the_title() ?></h1>
-        <p>
-            L’internat constitue pour tous les élèves un lieu de vie, 
-            d’hébergement, de travail, de repos et de détente dans le respect d’autrui. 
-            Il apporte à chacun la possibilité de réussir sa scolarité, il s’agit donc, 
-            avant tout, de respecter l’équilibre entre le travail, le sommeil et la 
-            détente de chacun pour le bien de tous.
-        </p>
+        <div class="presentation__content-entreprise">
+            <?php if(have_posts()) : 
+                while(have_posts()) : the_post(); ?> 
+                    <?php the_content(); ?> 
+                <?php endwhile;
+            endif; ?>
+        </div>
+    </div>
+    <div class="entreprises__faq">
+        <h2>Foire aux questions</h2>
+        <div>
+            <details>
+                <summary>Pourquoi embaucher un alternant ?</summary>
+                Embaucher un alternant peut...
+            </details>
+            <details>
+                <summary>Pourquoi embaucher un alternant ?</summary>
+                Embaucher un alternant peut...
+            </details>
+            <details>
+                <summary>Pourquoi embaucher un alternant ?</summary>
+                Embaucher un alternant peut...
+            </details>
+        </div>
+    </div>
+    <div class="entreprises__contact">
+        <h2>Contact</h2>
+        <div>
+            <p>Pour plus d’informations sur la taxe d’apprentissage, veuillez contacter Mme LEGRAND</p>
+            <button href="https://lyceecousteau.prod.louis-boulanger.fr/contactez-nous/">Nous Contacter</button>
+        </div>
     </div>
 </div>
 
